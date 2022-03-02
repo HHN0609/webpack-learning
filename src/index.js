4212b0237ddac59dfe0d8437ad1ed98ee32d9a7e
HHN0609 <1446909703@qq.com>
2022-03-02 20:41:23 +0800

    设置对外部资源的prefetch和preload

--- src/index.js
+++ src/index.js
@@ -30,7 +30,11 @@ const button = document.createElement('button')
 button.textContent = "click me"
 button.addEventListener("click", () => {
     // 用import来对资源进行懒加载
-    import('./math.js')
+    // 在import里面添加“魔法注释”，来实现额外的功能
+    // prefetch: 会在主页面加载完成后有空闲时，加载math.bundle.js，对应脚本的<script>标签上会多一个ref=prefetch
+    // 这里的prefetch的脚本在被加载时候，返回的状态码是304
+    // preload: 
+    import(/*webpackChunkName: 'math', webpackPrefetch: true*/'./math.js')
         .then(({ add }) => {
             let res = add(10, 11)
             console.log(res)
