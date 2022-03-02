a18f425b1480e297bdd47cb1a34fa67e852ea263
HHN0609 <1446909703@qq.com>
2022-03-02 22:42:49 +0800

    进一步提取development和production配置的公共配置，并使用webpack-merge来合并配置文件

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,108 +1,10 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const yaml = require('yaml')
-
 module.exports = {
-    entry: {
-        index: path.join(__dirname, '../src/index.js'),
-        another: path.join(__dirname, '../src/another-module.js')
-    },
     output: {
-        filename: 'scripts/[name].js',
-        path: path.join(__dirname, '../dist'),
-        clean: true,
-        assetModuleFilename: 'images/[contenthash].[ext]',
+        filename: 'scripts/[name].js'
     },
     mode: 'development',
     devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html', 
-            filename: 'app.html',
-            inject: 'body'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'style/[contenthash].css'
-        })
-    ],
     devServer: {
         static: './dist'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.png$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/test.png'
-                }
-            },
-            {
-                test: /\.svg$/,
-                type: 'asset/inline'
-            },
-            {
-                test: /\.txt$/,
-                type: 'asset/source'
-            },
-            {
-                test: /\.jpg$/,
-                type: 'asset',
-                parser:{
-                    dataUrlCondition: {
-                        maxSize: 4 * 1024 * 1024
-                    }
-                }
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(css)$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.xml$/,
-                use: 'xml-loader'
-            },
-            {
-                test: /\.yaml$/,
-                type: 'json',
-                parser: {
-                    parse: yaml.parse
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: [
-                            [
-                                '@babel/plugin-transform-runtime'
-                            ]
-                        ]
-                    }
-                }
-            }
-        ]
-    },
-    optimization: {
-        // 用webpack自带的代码split工具对代码进行抽离
-        splitChunks: {
-            // chunks: 'all'
-            cacheGroups: {
-                // 设置cacheGroups可以第三方的库打包进一个文件，进行缓存，因为这些库并不是要经常修改，就可以把这些第三方的库缓存到本地
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
-                }
-            }
-        }
     }
 }
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,106 +1,19 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
 const TerserWebpackPlugin = require('terser-webpack-plugin')
-const yaml = require('yaml')
 
 module.exports = {
-    entry: {
-        index: path.join(__dirname, '../src/index.js'),
-        another: path.join(__dirname, '../src/another-module.js')
-    },
     output: {
         filename: 'scripts/[name].[contenthash].js',
-        path: path.join(__dirname, '../dist'),
-        clean: true,
-        assetModuleFilename: 'images/[contenthash].[ext]',
         publicPath: 'http://localhost:8080/' 
     },
     mode: 'production',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html', 
-            filename: 'app.html',
-            inject: 'body'
-        }),
-        new MiniCssExtractPlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.png$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/test.png'
-                }
-            },
-            {
-                test: /\.svg$/,
-                type: 'asset/inline'
-            },
-            {
-                test: /\.txt$/,
-                type: 'asset/source'
-            },
-            {
-                test: /\.jpg$/,
-                type: 'asset',
-                parser:{
-                    dataUrlCondition: {
-                        maxSize: 4 * 1024 * 1024
-                    }
-                }
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(css)$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.xml$/,
-                use: 'xml-loader'
-            },
-            {
-                test: /\.yaml$/,
-                type: 'json',
-                parser: {
-                    parse: yaml.parse
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: [
-                            [
-                                '@babel/plugin-transform-runtime'
-                            ]
-                        ]
-                    }
-                }
-            }
-        ]
-    },
     optimization: {
         minimizer: [
             new CssMinimizerWebpackPlugin(),
             new TerserWebpackPlugin() // 对js代码进行压缩, 只有production环境有用
-        ],
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
-                }
-            }
-        }
+        ]
+    },
+    performance: {
+        hints: false
     }
 }
